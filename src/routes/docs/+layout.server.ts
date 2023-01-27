ebd527a1e5f3d46d462506e3c0464bbafdc4edae
Amilcar Munoz <yellowballpython@gmail.com>
2023-01-26 23:14:20 -0600

    resolve doc file paths

--- src/routes/docs/+layout.server.ts
+++ src/routes/docs/+layout.server.ts
@@ -1,12 +1,14 @@
 import type { LayoutServerLoad } from '../$types';
 import { readFile } from 'fs/promises';
+import { resolve } from 'path';
 export const load = (async (event) => {
 	const session = await event.locals.getSession();
 
 	if (!event.params.page) event.params.page = 'home';
 
 	try {
-		const content = await readFile(paths[event.params.page as keyof typeof paths], 'utf-8');
+		const path = resolve('src/data/docs', `${event.params.page}.md`);
+		const content = await readFile(path, 'utf-8');
 
 		return {
 			md: content,
@@ -16,13 +18,3 @@ export const load = (async (event) => {
 		return { session };
 	}
 }) satisfies LayoutServerLoad;
-
-const root = './src/data/docs';
-
-const paths = {
-	home: `${root}/home.md`,
-	tests: `${root}/tests.md`,
-	argumentos: `${root}/argumentos.md`,
-	retos: `${root}/retos.md`,
-	'funcion-inicial': `${root}/funcion-inicial.md`
-};
